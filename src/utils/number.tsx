a57d5751b38a76379ae8ae4950573487f5088db7
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-03-04 13:24:18 +0700

    new update

--- src/utils/number.tsx
+++ src/utils/number.tsx
@@ -2,7 +2,7 @@ export function toCurrency(number: string|number) {
 	let numberSample: string|number;
 	if(typeof number === "number") numberSample = number.toString()
 	numberSample = number
-	let currencyString = number.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+	let currencyString = numberSample.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 	currencyString = currencyString.replace("$", "")
 	currencyString = currencyString.replace(".00", "")
 
